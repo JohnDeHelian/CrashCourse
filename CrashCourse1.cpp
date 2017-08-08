88c7c82f7b96c45ef90620d000a3e2bfe81f3366
John DeHelian <john.de.helian@cranepi.com>
2017-08-08 08:24:05 -0400

    Made a change in branch from stub

--- CrashCourse1.cpp
+++ CrashCourse1.cpp
@@ -6,6 +6,7 @@ int main (int argc, char *argv[])
 {
     cout << "hello world" << endl;
     /* This is change in master branch */
+    /* Now I'm making a change in branch from stub */
     return(0);
 }
 
