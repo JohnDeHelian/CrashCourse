33d8e28a24f5192506e376b9e29d55be2c8709e5
John DeHelian <john.de.helian@cranepi.com>
2017-08-08 08:58:01 -0400

    Add new module to addMoreFiles branch and update CrashCourse1.cpp file

--- CrashCourse1.cpp
+++ CrashCourse1.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 
 using namespace std;
-
+extern int testFunction();
 int main (int argc, char *argv[])
 {
     cout << "hello world" << endl;
     /* This is change in master branch */
     /* This is a change in AddMoreFiles branch */
     /* This is a merge from master to addMoreFiiles I think */
+    testFunction();
     return(0);
 }
 
---
+++ newFile.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int testFunction ()
+{
+    cout << "hello testFunction" << endl;
+    return(0);
+}
+
+
